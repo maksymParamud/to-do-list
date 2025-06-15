a4d91e6453a54d4309f24dcc9105ed865854fa79
maksym <paramudmaksym72@gmail.com>
2025-06-15 20:56:08 +0300

    added eventListener to btn and fixed some bugs

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const input = document.querySelector("#input-to-do-list");
 const btn = document.querySelector("#btn-submit");
 const list = document.querySelector("#nav");
+
+btn.addEventListener("click", () => {
+  const li = document.createElement("li");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "delete";
+  li.innerHTML = input.value;
+  list.appendChild(li);
+  li.appendChild(deleteBtn);
+});
