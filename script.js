de1ae29f311acff79e447027ce61717f4c251227
maksym <paramudmaksym72@gmail.com>
2025-06-15 21:50:42 +0300

    fixed prevent empty tasks and clear input after adding

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const btn = document.querySelector("#btn-submit");
 const list = document.querySelector("#nav");
 
 btn.addEventListener("click", () => {
+  const taskText = input.value.trim();
+
+  if (taskText === "") return;
+
   const li = document.createElement("li");
   li.className = "todo-list-item";
+  li.innerText = taskText;
+
   const deleteBtn = document.createElement("button");
   deleteBtn.className = "delete-btn";
   deleteBtn.innerText = "delete";
-  li.innerHTML = input.value;
-  list.appendChild(li);
+
   li.appendChild(deleteBtn);
+  list.appendChild(li);
+
+  input.value = "";
 });
