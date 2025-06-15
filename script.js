a3e575c0ba622d169bf085b2730c360723bc16c3
maksym <paramudmaksym72@gmail.com>
2025-06-15 21:36:13 +0300

    added styles for todo list items

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const list = document.querySelector("#nav");
 
 btn.addEventListener("click", () => {
   const li = document.createElement("li");
+  li.className = "todo-list-item";
   const deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete-btn";
   deleteBtn.innerText = "delete";
   li.innerHTML = input.value;
   list.appendChild(li);
